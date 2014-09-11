31a5b54d7f27d7a61608cc17788d414357003b6e
Saurabh Purnaye <spurnaye@gmail.com>
2014-09-11 16:29:55 -0400

    removing unwanted services

--- lib/active_fulfillment/fulfillment/services.rb
+++ lib/active_fulfillment/fulfillment/services.rb
@@ -1,5 +1,1 @@
-require 'active_fulfillment/fulfillment/services/shopify_api'
-require 'active_fulfillment/fulfillment/services/shipwire'
-require 'active_fulfillment/fulfillment/services/webgistix'
-require 'active_fulfillment/fulfillment/services/amazon'
-require 'active_fulfillment/fulfillment/services/amazon_mws'
+require 'active_fulfillment/fulfillment/services/cafe_press'

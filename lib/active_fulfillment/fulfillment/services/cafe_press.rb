931bdd99f5925cb8ff22dfa7fb58772ac666cb88
Saurabh Purnaye <spurnaye@gmail.com>
2014-09-11 16:02:46 -0400

    adding code to connect to Cafe Press

--- lib/active_fulfillment/fulfillment/services/cafe_press.rb
+++ lib/active_fulfillment/fulfillment/services/cafe_press.rb
@@ -8,56 +8,22 @@ module ActiveMerchant
       attr_accessor :client
 
       def initialize(options = {})
-      	check_test_mode(options)
         initialize_ezp_client(options)
         @options = {}
         @options.update(options)
       end
 
-      def test_mode?
-        false
-      end
-
-      def test?
-        @options[:test] || Base.mode == :test
-      end
-
-      def valid_credentials?
-        true
-      end
-
       def fulfill(order_id, shipping_address, line_items, options = {})
         requires!(options, :customer, :order)
-        client.place_order(options[:customer], options[:order], line_items, shipping_address)
-      end
-
-      def fetch_stock_levels(options = {})
-        raise NotImplementedError.new("Subclasses must implement")
-      end
-
-      def fetch_tracking_numbers(order_ids, options = {})
-        response = fetch_tracking_data(order_ids, options)
-        response.params.delete('tracking_companies')
-        response.params.delete('tracking_urls')
-        response
-      end
-
-      def fetch_tracking_data(order_ids, options = {})
-        raise NotImplementedError.new("Subclasses must implement")
+        @client.place_order(options[:customer], options[:order], line_items, shipping_address)
       end
 
       private
-
       def initialize_ezp_client(options)
-      	requires!(options, :partner_id, :vendor)
-        @client = Client.new(partner_id, vendor)
+        requires!(options, :partner_id, :vendor)
+        @client = Client.new(options[:partner_id], options[:vendor])
       end
 
-      def check_test_mode(options)
-        if options[:test] and not test_mode?
-          raise ArgumentError, 'Test mode is not supported by this gateway'
-        end
-      end
     end
   end
 end

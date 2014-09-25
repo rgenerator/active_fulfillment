26ee7f96e36102fb960f87583aab810dbb5f1b48
Saurabh Purnaye <spurnaye@gmail.com>
2014-09-25 10:59:46 -0400

    making changes to the names of methods to match active_ff

--- lib/active_fulfillment/fulfillment/services/cafe_press.rb
+++ lib/active_fulfillment/fulfillment/services/cafe_press.rb
@@ -4,16 +4,29 @@ include CafePress::SimpleOrderAPI
 module ActiveMerchant
   module Fulfillment
     class CafePress < Service
-      cattr_accessor :logger
-      attr_accessor :client
+      attr_accessor :client, :partner_id
 
-      def initialize(partner_id)
-        @client = Client.new(partner_id)
+      def initialize(partner_id, options = {})
+        @partner_id = partner_id
+        @client = Client.new(partner_id, options)
+        super
       end
 
       def fulfill(order_id, shipping_address, line_items, options = {})
-        requires!(options, :customer, :order)
-        @client.place_order(options[:customer], options[:order], line_items, shipping_address)
+        requires!(options, :order)
+        @client.create_order(order_id, shipping_address, line_items, options)
+      end
+
+       def fetch_tracking_data(express_order_id, options = {})
+        @client.get_shipping_info(express_order_id, options)
+      end
+
+      def get_order_status(express_order_id, options = {})
+         @client.get_order_status(express_order_id, options)
+      end
+
+      def cancel_order(express_order_id, options = {})
+        @client.cancel_order(express_order_id, options)
       end
 
     end

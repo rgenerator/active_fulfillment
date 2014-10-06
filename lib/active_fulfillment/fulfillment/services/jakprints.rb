5424cd7ea70fdc255edc6365ad6f4c042d4c36a2
sshaw <skye.shaw@gmail.com>
2014-10-06 10:40:01 -0400

    WIP: Jakprints fulfiller

--- lib/active_fulfillment/fulfillment/services/jakprints.rb
+++ lib/active_fulfillment/fulfillment/services/jakprints.rb
@@ -1,12 +1,15 @@
-require 'cafe_press/simple_order_api/client'
+require 'jakprints'
+
 include CafePress::SimpleOrderAPI
 
 module ActiveMerchant
   module Fulfillment
-    class CafePress < Service
+    class Jakprints < Service
       attr_accessor :client, :partner_id
 
       def initialize(options = {})
+        super
+
         requires!(options, :partner_id)
         @partner_id = options[:partner_id]
         options.delete(:partner_id)
@@ -15,16 +18,17 @@ module ActiveMerchant
       end
 
       def fulfill(order_id, shipping_address, line_items, options = {})
+        
         response = @client.create_order(order_id, shipping_address, line_items, options)
         Response.new(true, "Order created: #{response[:order_no]}", response)
       rescue => e # something
         Response.new(false, e.to_s, response)
       end
 
-       def fetch_tracking_data(express_order_id, options = {})
-         begin
-           response = @client.get_shipping_info(express_order_id, options)
-            Response.new(true, "Get Shipment Info response", response)
+      def fetch_tracking_data(express_order_id, options = {})
+        begin
+          response = @client.get_shipping_info(express_order_id, options)
+          Response.new(true, "Get Shipment Info response", response)
         rescue => e # something
           Response.new(false, e.to_s, response)
         end
@@ -34,32 +38,10 @@ module ActiveMerchant
         raise NotImplementedError, 'fetch_stock_levels is not implemented'
       end
 
-      ###########
-      # MP specific
-      ###########
-
-      def get_order_status(express_order_id, options = {})
-        begin
-          response = @client.get_order_status(express_order_id, options)
-          Response.new(true, "Get Order Status response", response)
-        rescue => e # something
-          Response.new(false, e.to_s, response)
-        end
-      end
-
       def cancel_order(express_order_id, options = {})
         begin
           response = @client.cancel_order(express_order_id, options)
           Response.new(true, "Cancel Order response", response)
-        rescue => e # something
-          Response.new(false, e.to_s, response)
-        end
-      end
-
-      def get_order_by_internal_id(identiciation_code, internal_order_id, options = {})
-         begin
-          response = @client.get_order_by_secondary_identifier(identiciation_code, internal_order_id, options)
-          Response.new(true, "Get Shipment Info response", response)
         rescue => e # something
           Response.new(false, e.to_s, response)
         end

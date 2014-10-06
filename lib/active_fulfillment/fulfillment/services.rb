49e3183a309c9246b82246b5eb4545a45da295e3
Kwadwo Owusu <mr_kwadwo.owusu@yahoo.com>
2014-10-06 17:52:56 -0400

    added the jakprints service

--- lib/active_fulfillment/fulfillment/services.rb
+++ lib/active_fulfillment/fulfillment/services.rb
@@ -1,2 +1,3 @@
-#currently supporting only Cafe Press!!
+#currently supporting only Cafe Press & Jakprints!!
 require 'active_fulfillment/fulfillment/services/cafe_press'
+require 'active_fulfillment/fulfillment/services/jakprints'
--- lib/active_fulfillment/fulfillment/services/jakprints.rb
+++ lib/active_fulfillment/fulfillment/services/jakprints.rb
@@ -1,36 +1,34 @@
 require 'jakprints'
 
-include CafePress::SimpleOrderAPI
-
 module ActiveMerchant
   module Fulfillment
     class Jakprints < Service
       attr_accessor :client, :partner_id
 
       def initialize(options = {})
-        super
-
-        requires!(options, :partner_id)
-        @partner_id = options[:partner_id]
-        options.delete(:partner_id)
-        @client = Client.new(partner_id, options)
+        requires!(options, :url, :username, :password)
+        Jakprints.configure options
         super
       end
 
       def fulfill(order_id, shipping_address, line_items, options = {})
-        
-        response = @client.create_order(order_id, shipping_address, line_items, options)
-        Response.new(true, "Order created: #{response[:order_no]}", response)
+        requires!(shipping_address, :first_name, :last_name, :address1, :city, :state, :zip_code)
+        line_items.each do |item|
+          requires!(item, :sku, :quantity)
+        end
+        request = build_fulfillment_request(order_id, shipping_address, line_items)
+        created_order = Jakprints::Order.add_order(request)
+        Response.new(true, "Order created: #{created_order.Order['id']}", created_order.attributes)
       rescue => e # something
-        Response.new(false, e.to_s, response)
+        Response.new(false, e.to_s, created_order)
       end
 
-      def fetch_tracking_data(express_order_id, options = {})
+      def fetch_tracking_data(jakprints_order_id, options = {})
         begin
-          response = @client.get_shipping_info(express_order_id, options)
-          Response.new(true, "Get Shipment Info response", response)
+          order_response = Jakprints::Order.get_by_id(jakprints_order_id, options)
+          Response.new(true, "Get Shipment Info response", order_response.attributes)
         rescue => e # something
-          Response.new(false, e.to_s, response)
+          Response.new(false, e.to_s, order_response)
         end
       end
 
@@ -39,11 +37,22 @@ module ActiveMerchant
       end
 
       def cancel_order(express_order_id, options = {})
-        begin
-          response = @client.cancel_order(express_order_id, options)
-          Response.new(true, "Cancel Order response", response)
-        rescue => e # something
-          Response.new(false, e.to_s, response)
+        raise NotImplementedError, 'cancel_order is not implemented'
+      end
+      
+      private
+      def build_fulfillment_request(order_id, shipping_address, line_items)
+        request = {}
+        request['Order']['client_ref'] = order_id
+        request['Shipment']['first'] = shipping_address['first_name']
+        request['Shipment']['last'] = shipping_address['last_name']
+        request['Shipment']['address1'] = shipping_address['address1']
+        request['Shipment']['address2'] = shipping_address['address2'] if shipping_address['address2']
+        request['Shipment']['city'] = shipping_address['city']
+        request['Shipment']['state'] = shipping_address['state']
+        request['Shipment']['zip'] = shipping_address['zip_code']
+        request['OrderItems'] = line_items.collect do |item|
+          { 'sku' => item['sku'], 'quantity' => item['quantity']}
         end
       end
 

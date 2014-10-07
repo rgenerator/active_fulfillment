f871c1dfc3e9b46393973131ace02ce2c1b84bff
Kwadwo Owusu <mr_kwadwo.owusu@yahoo.com>
2014-10-07 17:38:20 -0400

    make Jakprints api calls successfully

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source "https://rubygems.org"
 gem 'cafe_press-simple_order_api', :git => 'git@github.com:rgenerator/cafe_press-simple_order_api.git'
+gem 'jakprints-client', :git => 'git@github.com:rgenerator/jakprints-client.git'
 gemspec
--- lib/active_fulfillment/fulfillment/services/jakprints.rb
+++ lib/active_fulfillment/fulfillment/services/jakprints.rb
@@ -7,7 +7,7 @@ module ActiveMerchant
 
       def initialize(options = {})
         requires!(options, :url, :username, :password)
-        Jakprints.configure options
+        ::Jakprints.configure options
         super
       end
 
@@ -16,18 +16,24 @@ module ActiveMerchant
         line_items.each do |item|
           requires!(item, :sku, :quantity)
         end
+        binding.pry
         request = build_fulfillment_request(order_id, shipping_address, line_items)
-        created_order = Jakprints::Order.add_order(request)
+        binding.pry
+        created_order = ::Jakprints::Order.add_order(request)
+        binding.pry
         Response.new(true, "Order created: #{created_order.Order['id']}", created_order.attributes)
       rescue => e # something
+        created_order = created_order || {}
+        binding.pry
         Response.new(false, e.to_s, created_order)
       end
 
       def fetch_tracking_data(jakprints_order_id, options = {})
         begin
-          order_response = Jakprints::Order.get_by_id(jakprints_order_id, options)
+          order_response = ::Jakprints::Order.get_by_id(jakprints_order_id)
           Response.new(true, "Get Shipment Info response", order_response.attributes)
         rescue => e # something
+          order_response = order_response || {}
           Response.new(false, e.to_s, order_response)
         end
       end
@@ -42,18 +48,22 @@ module ActiveMerchant
       
       private
       def build_fulfillment_request(order_id, shipping_address, line_items)
+        binding.pry
         request = {}
-        request['Order']['client_ref'] = order_id
-        request['Shipment']['first'] = shipping_address['first_name']
-        request['Shipment']['last'] = shipping_address['last_name']
-        request['Shipment']['address1'] = shipping_address['address1']
-        request['Shipment']['address2'] = shipping_address['address2'] if shipping_address['address2']
-        request['Shipment']['city'] = shipping_address['city']
-        request['Shipment']['state'] = shipping_address['state']
-        request['Shipment']['zip'] = shipping_address['zip_code']
-        request['OrderItems'] = line_items.collect do |item|
-          { 'sku' => item['sku'], 'quantity' => item['quantity']}
+        request[:Order] = { :client_ref => order_id }
+        request[:Shipment] = {}
+        request[:Shipment][:first] = shipping_address[:first_name] 
+        request[:Shipment][:last] = shipping_address[:last_name] 
+        request[:Shipment][:address1] = shipping_address[:address1] 
+        request[:Shipment][:address2] = shipping_address[:address2] if shipping_address[:address2]
+        request[:Shipment][:city] = shipping_address[:city] 
+        request[:Shipment][:state] = shipping_address[:state] 
+        request[:Shipment][:zip] = shipping_address[:zip_code] 
+        binding.pry
+        request[:OrderItems] = line_items.collect do |item|
+          { :sku => item[:sku], :quantity => item[:quantity]}
         end
+        request
       end
 
     end

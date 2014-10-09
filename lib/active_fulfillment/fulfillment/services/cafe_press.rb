f924f47133bfafb91a71d823f9fc0e8134443e76
sshaw <skye.shaw@gmail.com>
2014-10-08 22:08:58 -0400

    On exceptions we have no response

--- lib/active_fulfillment/fulfillment/services/cafe_press.rb
+++ lib/active_fulfillment/fulfillment/services/cafe_press.rb
@@ -18,15 +18,15 @@ module ActiveMerchant
         response = @client.create_order(order_id, shipping_address, line_items, options)
         Response.new(true, "Order created: #{response[:order_no]}", response)
       rescue => e # something
-        Response.new(false, e.to_s, response)
+        Response.new(false, e.to_s)
       end
 
        def fetch_tracking_data(express_order_id, options = {})
          begin
            response = @client.get_shipping_info(express_order_id, options)
-            Response.new(true, "Get Shipment Info response", response)
+           Response.new(true, "Get Shipment Info response", response)
         rescue => e # something
-          Response.new(false, e.to_s, response)
+          Response.new(false, e.to_s)
         end
       end
 
@@ -43,7 +43,7 @@ module ActiveMerchant
           response = @client.get_order_status(express_order_id, options)
           Response.new(true, "Get Order Status response", response)
         rescue => e # something
-          Response.new(false, e.to_s, response)
+          Response.new(false, e.to_s)
         end
       end
 
@@ -52,7 +52,7 @@ module ActiveMerchant
           response = @client.cancel_order(express_order_id, options)
           Response.new(true, "Cancel Order response", response)
         rescue => e # something
-          Response.new(false, e.to_s, response)
+          Response.new(false, e.to_s)
         end
       end
 
@@ -61,7 +61,7 @@ module ActiveMerchant
           response = @client.get_order_by_secondary_identifier(identiciation_code, internal_order_id, options)
           Response.new(true, "Get Shipment Info response", response)
         rescue => e # something
-          Response.new(false, e.to_s, response)
+          Response.new(false, e.to_s)
         end
       end
 

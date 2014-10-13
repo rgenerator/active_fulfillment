ba3bd7261d01748f077140a0a970ebbbbb2c866a
sshaw <skye.shaw@gmail.com>
2014-10-13 13:31:30 -0400

    Proper support for test_mode, don't touch caller's Hash

--- lib/active_fulfillment/fulfillment/services/cafe_press.rb
+++ lib/active_fulfillment/fulfillment/services/cafe_press.rb
@@ -8,11 +8,13 @@ module ActiveMerchant
       attr_accessor :client, :partner_id
 
       def initialize(options = {})
-        requires!(options, :partner_id)
-        @partner_id = options[:partner_id]
-        options.delete(:partner_id)
-        @client = Client.new(partner_id, options)
         super
+
+        options = options.dup
+        requires!(options, :partner_id)
+
+        @partner_id = options.delete(:partner_id)
+        @client = Client.new(partner_id, options)
       end
 
       def fulfill(order_id, shipping_address, line_items, options = {})
@@ -33,6 +35,11 @@ module ActiveMerchant
 
       def fetch_stock_levels(options = {})
         raise NotImplementedError, 'fetch_stock_levels is not implemented'
+      end
+
+      # Yes, there's a test environment
+      def test_mode?
+        true
       end
 
       ###########

ce41e4c1fbeab451ab392bf85af9140ce5b62b99
Kwadwo Owusu <mr_kwadwo.owusu@yahoo.com>
2014-10-17 17:15:25 -0400

    remove unused test methods for jakprints

--- test/unit/services/jakprints_test.rb
+++ test/unit/services/jakprints_test.rb
@@ -43,13 +43,6 @@ class JakprintsTest < Test::Unit::TestCase
 
   private
 
-  def build_mock_response(response, message, code = "200")
-    http_response = mock(:code => code, :message => message)
-    http_response.stubs(:body).returns(response)
-    http_response
-  end
-
-
   def successful_fulfillment_response
     {"Order"=>{"id"=>"37414", "order_date"=>"2014-09-17 11:42:49", "client_ref"=>"11234", "status_shipped"=>false, "status_invoiced"=>false, "status_new"=>false, "status_hold"=>false, "status_cancelled"=>false},
    "Shipment"=>{"first"=>"Bob", "last"=>"Saget", "attention"=>"", "address1"=>"820 Crest Ave", "address2"=>"Apt 66", "city"=>"Queens", "state"=>"NY", "zip"=>"14033", "trackingnumber"=>nil, "shipdate"=>nil, "client_ref"=>""},
@@ -62,7 +55,7 @@ class JakprintsTest < Test::Unit::TestCase
       "order_id"=>"37414",
       "Product"=>
        {"id"=>"17034",
-        "sku"=>"121212",
+        "sku"=>"999777666561",
         "description"=>"Test Product 1",
         "substrate_id"=>"170",
         "Substrate"=>{"id"=>"170", "manufacturer"=>"Alstyle", "manufacturer_number"=>"1301", "color"=>"black", "size"=>"sm"},

28bf732a198eda754d8d1a949d24712f7b9fbd4c
Saurabh Purnaye <spurnaye@gmail.com>
2014-11-10 16:51:39 -0500

    adding more granular exceptions for Cafepress communication

--- lib/active_fulfillment/fulfillment/services/cafe_press.rb
+++ lib/active_fulfillment/fulfillment/services/cafe_press.rb
@@ -1,47 +1,47 @@
-require 'cafe_press/simple_order_api'
+require 'cafe_press/simple_order_api/client'
+include CafePress::SimpleOrderAPI
 
 module ActiveMerchant
   module Fulfillment
     class CafePress < Service
-      include ::CafePress::SimpleOrderAPI
-
       attr_accessor :client, :partner_id
 
       def initialize(options = {})
-        super
-
-        options = options.dup
         requires!(options, :partner_id)
-
-        options[:test] = true if test?
-
-        @partner_id = options.delete(:partner_id)
+        @partner_id = options[:partner_id]
+        options.delete(:partner_id)
         @client = Client.new(partner_id, options)
+        super
       end
 
       def fulfill(order_id, shipping_address, line_items, options = {})
-        response = @client.create_order(order_id, shipping_address, line_items, options)
-        Response.new(true, "Order created: #{response[:order_no]}", response)
-      rescue Error => e
-        Response.new(false, e.to_s)
+        send_request do
+          response = @client.create_order(order_id, shipping_address, line_items, options)
+          Response.new(true, "Order created: #{response[:order_no]}", response)
+        end
+      end
+
+      def test_mode?
+        true
       end
 
        def fetch_tracking_data(express_order_id, options = {})
-         begin
+         send_request do
            response = @client.get_shipping_info(express_order_id, options)
            Response.new(true, "Get Shipment Info response", response)
-         rescue Error => e
-          Response.new(false, e.to_s)
-        end
+         end
+      end
+
+      def send_request
+        yield
+      rescue ::CafePress::SimpleOrderAPI::InvalidRequestError => e
+        Response.new(false, e.to_s)
+      rescue ::CafePress::SimpleOrderAPI::ConnectionError => e
+        raise ActiveMerchant::ConnectionError, e.to_s
       end
 
       def fetch_stock_levels(options = {})
         raise NotImplementedError, 'fetch_stock_levels is not implemented'
-      end
-
-      # Yes, there's a test environment
-      def test_mode?
-        true
       end
 
       ###########
@@ -49,29 +49,23 @@ module ActiveMerchant
       ###########
 
       def get_order_status(express_order_id, options = {})
-        begin
+        send_request do
           response = @client.get_order_status(express_order_id, options)
           Response.new(true, "Get Order Status response", response)
-        rescue Error => e
-          Response.new(false, e.to_s)
         end
       end
 
       def cancel_order(express_order_id, options = {})
-        begin
+        send_request do
           response = @client.cancel_order(express_order_id, options)
           Response.new(true, "Cancel Order response", response)
-        rescue Error => e
-          Response.new(false, e.to_s)
         end
       end
 
       def get_order_by_internal_id(identiciation_code, internal_order_id, options = {})
-         begin
+         send_request do
           response = @client.get_order_by_secondary_identifier(identiciation_code, internal_order_id, options)
           Response.new(true, "Get Shipment Info response", response)
-        rescue Error => e
-          Response.new(false, e.to_s)
         end
       end
 
